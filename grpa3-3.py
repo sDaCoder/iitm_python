e8e922d2f50e95eea2582679bdb44a6077376cd3
sDaCoder <supravadutta_sDa@outlook.com>
2024-10-15 22:09:31 +0530

    OOPs

--- grpa3-3.py
+++ grpa3-3.py
@@ -18,55 +18,57 @@
 
 task = input()
 
-def permutation(s: str) -> None:
-    for i in range(len(s)):
-        for j in range(len(s)):
-            if i != j:
-                print(s[i] + s[j])
-    
-def sorted_permutation(s: str) -> None:
-    for i in range(len(s)):
-        for j in range(len(s)):
-            if i != j and s[i] < s[j]:
-                print(s[i] + s[j])
-    
-def repeat_the_repeat(n: int) -> None:
-    for j in range(n):
+class Solution:
+    def permutation(self, s: str) -> None:
+        for i in range(len(s)):
+            for j in range(len(s)):
+                if i != j:
+                    print(s[i] + s[j])
+        
+    def sorted_permutation(self, s: str) -> None:
+        for i in range(len(s)):
+            for j in range(len(s)):
+                if i != j and s[i] < s[j]:
+                    print(s[i] + s[j])
+        
+    def repeat_the_repeat(self, n: int) -> None:
+        for j in range(n):
+            for i in range(n):
+                print(i + 1, end = "")
+            print()
+        
+    def repeat_incrementally(self, n: int) -> None:
         for i in range(n):
-            print(i + 1, end = "")
-        print()
+            for j in range(i + 1):
+                print(j + 1, end = "")
+            print()
+        
+    def increment_and_decrement(self, n: int) -> None:
+        for i in range(1, n + 1):
+            a = 1
+            for j in range(1, 2 * i):
+                print(a, end = "")
+                if j >= i:
+                    a -= 1
+                else:
+                    a += 1
+            print()
     
-def repeat_incrementally(n: int) -> None:
-    for i in range(n):
-        for j in range(i + 1):
-            print(j + 1, end = "")
-        print()
-    
-def increment_and_decrement(n: int) -> None:
-    for i in range(1, n + 1):
-        a = 1
-        for j in range(1, 2 * i):
-            print(a, end = "")
-            if j >= i:
-                a -= 1
-            else:
-                a += 1
-        print()
-    
+S = Solution()
 if task == 'permutation':
     s = input()
-    permutation(s)
+    S.permutation(s)
 elif task == 'sorted_permutation':
     s = input()
-    sorted_permutation(s)
+    S.sorted_permutation(s)
 elif task == 'repeat_the_repeat':
     n = int(input())
-    repeat_the_repeat(n)
+    S.repeat_the_repeat(n)
 elif task == 'repeat_incrementally':
     n = int(input())
-    repeat_incrementally(n)
+    S.repeat_incrementally(n)
 elif task == 'increment_and_decrement':
     n = int(input())
-    increment_and_decrement(n)
+    S.increment_and_decrement(n)
 else:
     pass
--- grpa3-4.py
+++ grpa3-4.py
@@ -11,80 +11,83 @@ min = None
 
 task = input()
 
-def factors(n: int) -> None:
-    for i in range(1, n + 1):
-        if n % i == 0:
-            print(i)
+class Solution:
+        
+    def factors(self, n: int) -> None:
+        for i in range(1, n + 1):
+            if n % i == 0:
+                print(i)
 
-def find_min(n: int) -> int:
-    mini = int(input())
-    for i in range(1, n):
-        num = int(input())
-        if num < mini:
-            mini = num
-    return mini
+    def find_min(self, n: int) -> int:
+        mini = int(input())
+        for i in range(1, n):
+            num = int(input())
+            if num < mini:
+                mini = num
+        return mini
 
-def prime_check(n: int) -> bool:
-    isPrime = True
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            isPrime = False
-            break
-    return isPrime
+    def prime_check(self, n: int) -> bool:
+        isPrime = True
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                isPrime = False
+                break
+        return isPrime
 
-def is_sorted(s: str) -> bool:
-    isSorted = True
-    for i in range(len(s) - 1):
-        if s[i] > s[i + 1]:
-            isSorted = False
-    return isSorted
+    def is_sorted(self, s: str) -> bool:
+        isSorted = True
+        for i in range(len(s) - 1):
+            if s[i] > s[i + 1]:
+                isSorted = False
+        return isSorted
 
-def any_true(n: int) -> bool:
-    isDiv = False
-    for i in range(n):
-        num = int(input())
-        if num % 3 == 0:
-            isDiv = True
-    return isDiv
+    def any_true(self, n: int) -> bool:
+        isDiv = False
+        for i in range(n):
+            num = int(input())
+            if num % 3 == 0:
+                isDiv = True
+        return isDiv
 
-def calc_manhattan() -> int:
-    x, y = 0, 0
-    while True:
-        dir = input()
-        if dir == 'STOP':
-            break
-        if dir == 'UP':
-            y += 1
-        elif dir == 'DOWN':
-            y -= 1
-        elif dir == 'LEFT':
-            x -= 1
-        elif dir == 'RIGHT':
-            x += 1
-    return abs(x) + abs(y)
+    def calc_manhattan(self) -> int:
+        x, y = 0, 0
+        while True:
+            dir = input()
+            if dir == 'STOP':
+                break
+            if dir == 'UP':
+                y += 1
+            elif dir == 'DOWN':
+                y -= 1
+            elif dir == 'LEFT':
+                x -= 1
+            elif dir == 'RIGHT':
+                x += 1
+        return abs(x) + abs(y)
 
+S = Solution()
 if task == 'factors':
     n = int(input())
-    factors(n)
+    S.factors(n)
 
 elif task == 'find_min':
     n = int(input())
-    print(find_min(n))
+    print(S.find_min(n))
 
 elif task == 'prime_check':
     n = int(input())
-    print(prime_check(n))
+    print(S.prime_check(n))
 
 elif task == 'is_sorted':
     s = input()
-    print(is_sorted(s))
+    print(S.is_sorted(s))
 
 elif task == 'any_true':
     n = int(input())
-    print(any_true(n))
+    print(S.any_true(n))
 
 elif task == 'manhattan':
-    print(calc_manhattan())
+    print(S.calc_manhattan())
 
 else:
     pass
